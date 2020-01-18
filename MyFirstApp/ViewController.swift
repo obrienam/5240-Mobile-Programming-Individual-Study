3cf8863f78db16dbb4364f254c5a708c6ccb2608
Aidan O'Brien <obrienam@appstate.edu>
2020-01-18 17:07:40 -0500

    Experimented with the storyboard

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -13,16 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let label = UILabel()
-        label.frame = CGRect(x: 125, y: 125, width: 200, height: 80)
-        label.text = "This is a label"
-        view.addSubview(label)
-        
-        let button = UIButton()
-        button.frame=CGRect(x: 125, y: 300, width: 80, height: 80)
-        button.setTitle("Button", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
     }
 
 
