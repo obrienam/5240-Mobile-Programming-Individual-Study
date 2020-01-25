d00ea7b8865ce2ba1995636197cc1de754c9cec4
Aidan O'Brien <obrienam@appstate.edu>
2020-01-25 11:33:50 -0500

    Initial Commit

---
+++ ObjectApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectApp
+//
+//  Created by Aidan O'Brien on 1/25/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
