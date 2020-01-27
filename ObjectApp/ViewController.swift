8f75a9ad2500bf8617305bb451407ae84538385d
Aidan O'Brien <obrienam@appstate.edu>
2020-01-26 22:02:01 -0500

    Experimented with UI and constraints

--- ObjectApp/ViewController.swift
+++ ObjectApp/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func enterBttn(_ sender: Any) {
+        textField.text=textField.text?.capitalized
+    }
+    
 
 }
 
