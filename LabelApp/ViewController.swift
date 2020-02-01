a51f883247d61a7ed7804f6feccda7d22063cc21
Aidan O'Brien <obrienam@appstate.edu>
2020-01-31 21:32:23 -0500

    Started Ch. 7 Project,

--- LabelApp/ViewController.swift
+++ LabelApp/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func changeLabel(_ sender: UIButton) {
+        
+        myLabel.text = "Text created by Swift \ncode using Xcode"
+        myLabel.numberOfLines = 2
+        myLabel.font = UIFont(name: "Courier", size: 14)
+        myLabel.backgroundColor = UIColor.yellow
+        myLabel.isEnabled = false
 
+        }
+    
 
 }
 
