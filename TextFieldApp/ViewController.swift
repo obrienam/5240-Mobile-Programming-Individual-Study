e8aa435c2568a840e35bf410f160e37e60b906bf
Aidan O'Brien <obrienam@appstate.edu>
2020-02-01 21:44:45 -0500

    Initial Commit

---
+++ TextFieldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldApp
+//
+//  Created by Aidan O'Brien on 2/1/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
