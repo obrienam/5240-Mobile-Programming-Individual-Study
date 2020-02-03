be511e2cf9e7080dd780c0e135b0a2944df62979
Aidan O'Brien <obrienam@appstate.edu>
2020-02-03 18:51:58 -0500

    Finished slider app project in ch. 8

--- SliderApp/ViewController.swift
+++ SliderApp/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelValue: UILabel!
+    @IBOutlet var sliderValue: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeSlider(_ sender: UIButton) {
+        sliderValue.minimumValue = 1
+        sliderValue.maximumValue = 25
+        sliderValue.value = 7
+        sliderValue.minimumTrackTintColor = UIColor.red
+        sliderValue.maximumTrackTintColor=UIColor.green
+        sliderValue.thumbTintColor = UIColor.black
+    }
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        labelValue.text = "\(sliderValue.value)"
+    }
+    
 }
 
