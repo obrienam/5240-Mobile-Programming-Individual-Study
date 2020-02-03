f75d350ee6bdd90f252dadbce6b7afda3e1a6486
Aidan O'Brien <obrienam@appstate.edu>
2020-02-03 18:53:08 -0500

    Initial Commit

---
+++ ProgressApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgressApp
+//
+//  Created by Aidan O'Brien on 2/3/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
