ac97da62f78127c243449b246252ca621eb97abf
Aidan O'Brien <obrienam@appstate.edu>
2020-02-03 18:35:17 -0500

    Finished stepper app from ch. 8

--- StepperApp/ViewController.swift
+++ StepperApp/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelValue: UILabel!
+    @IBOutlet var stepperValue: UIStepper!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        labelValue.text="\(stepperValue.value)"
+    }
+    
+    @IBAction func changeStepper(_ sender: UIButton) {
+    
+        stepperValue.minimumValue = -10
+        stepperValue.maximumValue = -5
+        stepperValue.stepValue=0.5
+        stepperValue.isContinuous=true
+        stepperValue.autorepeat=true
+        stepperValue.wraps=true
+    }
+    
 }
 
