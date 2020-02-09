78181243cd051fe4aa64c5d8fa44f0cb11d2e1b6
Aidan O'Brien <obrienam@appstate.edu>
2020-02-09 10:47:23 -0500

    Finished image view project from chapter 9

--- ImageViewApp/ViewController.swift
+++ ImageViewApp/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled=true
+        
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            let touch = touches.first
+            if touch?.view == imageView {
+                print ("Touched")
+            }
+            else {
+            print ("Nothing ")
+            }
+        }
 
 
 }
