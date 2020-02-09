eb9a964f57b3ed9b308bc7c13d5dfe51b687f28a
Aidan O'Brien <obrienam@appstate.edu>
2020-02-09 10:30:50 -0500

    Initial Commit

---
+++ ImageViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewApp
+//
+//  Created by Aidan O'Brien on 2/9/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
