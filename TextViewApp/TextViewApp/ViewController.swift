5f5bd48978da61c375ff44a439c4193857b40fa0
Aidan O'Brien <obrienam@appstate.edu>
2020-02-09 20:39:00 -0500

    Finished textview app

--- TextViewApp/TextViewApp/ViewController.swift
+++ TextViewApp/TextViewApp/ViewController.swift
@@ -14,8 +14,32 @@ class ViewController: UIViewController {
     @IBOutlet var buttonObject: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textView.dataDetectorTypes=UIDataDetectorTypes.phoneNumber
+        textView.isEditable=false
+        textView.isSelectable=true
+        textView.text="This is an example of clickable text 7049368675"
+    NotificationCenter.default.addObserver(self,selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tap)    }
+    
+    @objc func dismissKeyboard() { view.endEditing(true)
     }
+    @objc func keyboardWillShow(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+    if self.view.frame.origin.y == 0 { self.view.frame.origin.y -=      keyboardSize.height
+            }
+        
+        }
+    }
+    @objc func keyboardWillHide(notification: NSNotification) { if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+            if self.view.frame.origin.y != 0 {
+                self.view.frame.origin.y = 0 }
+        }
+    }
+    
     @IBAction func buttonTapped(_ sender: Any) {
         if textView.isEditable == true {
             textView.isEditable = false
