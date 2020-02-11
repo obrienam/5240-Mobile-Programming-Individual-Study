49bd51674b68a69dffa24e675afc87b64714088d
Aidan O'Brien <obrienam@appstate.edu>
2020-02-10 21:50:29 -0500

    Finished Controller app

--- ControllApp/ControllApp/ViewController.swift
+++ ControllApp/ControllApp/ViewController.swift
@@ -11,14 +11,42 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var buttonObject: UIButton!
+    @IBOutlet var labelSwitch: UILabel!
+    @IBOutlet var switchObject: UISwitch!
+    @IBOutlet var segmentedControl: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        segmentedControl.insertSegment(withTitle: "Three", at: 2, animated: true)
+        segmentedControl.setWidth(50, forSegmentAt: 0)
+        segmentedControl.setWidth(50, forSegmentAt: 1)
+        segmentedControl.setWidth(50, forSegmentAt: 2)
+        
     }
 
 
     @IBAction func touchInside(_ sender: UIButton) {
         buttonObject.setTitle("New",for: UIControl.State.normal)
+        switchObject.onTintColor=UIColor.red
+        switchObject.tintColor=UIColor.blue
+    }
+    @IBAction func switchChanged(_ sender: UISwitch) {
+        if switchObject.isOn {
+            labelSwitch.text="On"
+        } else {
+            labelSwitch.text="Off"
+        }
+    }
+    @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+            case 0:
+                labelSwitch.text = "One"
+            case 1:
+                labelSwitch.text = "Two"
+            default:
+                labelSwitch.text = "Three"
+                
+        }
     }
 }
 
