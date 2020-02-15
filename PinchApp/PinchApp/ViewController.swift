2073f0b1754f50205152521513a5dadd90d0b513
Aidan O'Brien <obrienam@appstate.edu>
2020-02-15 16:58:05 -0500

    Started pinch app

---
+++ PinchApp/PinchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinchApp
+//
+//  Created by Aidan O'Brien on 2/15/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
