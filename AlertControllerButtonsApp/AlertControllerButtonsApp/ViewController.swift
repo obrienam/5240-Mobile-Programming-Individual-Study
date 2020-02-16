c3923576e3701df89cb331dcd22d067f2cbae3f5
Aidan O'Brien <obrienam@appstate.edu>
2020-02-16 11:22:42 -0500

    Started multi-button app

---
+++ AlertControllerButtonsApp/AlertControllerButtonsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertControllerButtonsApp
+//
+//  Created by Aidan O'Brien on 2/16/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
