9521b2d940a757dc0fb2a0a039053268ce7c57d5
Aidan O'Brien <obrienam@appstate.edu>
2020-02-15 21:19:47 -0500

    Finished first pinch app

--- PinchApp/PinchApp/ViewController.swift
+++ PinchApp/PinchApp/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var topImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pinchDetected(_ sender: UIPinchGestureRecognizer) {
+        topImageView.transform=CGAffineTransform(scaleX: sender.scale, y: sender.scale)
+    }
+    
 }
 
