df48f9d34f341cb4154fb04791945616d0569122
Aidan O'Brien <obrienam@appstate.edu>
2020-02-29 11:19:33 -0500

    Finished sectioned collection view app

---
+++ CollectionViewSectionApp/CollectionViewSectionApp/itemCell.swift
@@ -0,0 +1,14 @@
+//
+//  itemCell.swift
+//  CollectionViewSectionApp
+//
+//  Created by Aidan O'Brien on 2/29/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UICollectionViewCell {
+    @IBOutlet var headerLabel: UILabel!
+    @IBOutlet var itemLabel: UILabel!
+}
