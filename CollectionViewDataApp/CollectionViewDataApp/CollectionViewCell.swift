0975c4d1264e0f15898d34742dcf325d572c64d9
Aidan O'Brien <obrienam@appstate.edu>
2020-02-28 22:07:04 -0500

    Finished Data collection view project

---
+++ CollectionViewDataApp/CollectionViewDataApp/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewDataApp
+//
+//  Created by Aidan O'Brien on 2/28/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var itemLabel:UILabel!
+}
---
+++ CollectionViewDataApp/CollectionViewDataApp/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  CollectionViewDataApp
+//
+//  Created by Aidan O'Brien on 2/28/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
+
+    @IBOutlet var collectionView: UICollectionView!
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        petArray.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! CollectionViewCell
+        cell.itemLabel.text = petArray[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "Your Choice", message: "\(petArray[indexPath.row])", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+}
+
