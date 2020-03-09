d03b2aa2cb029fd12c57f306e2a7aac5bec51577
Aidan O'Brien <obrienam@appstate.edu>
2020-03-09 16:24:04 -0400

    Added tab bar

---
+++ TabBarApp/TabBarApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarApp
+//
+//  Created by Aidan O'Brien on 3/9/20.
+//  Copyright © 2020 Aidan O'Brien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
