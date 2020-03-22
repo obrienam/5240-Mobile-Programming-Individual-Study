aec859b349af209da55e55b91a12e1f58be717c7
Aidan O'Brien <obrienam@appstate.edu>
2020-03-22 14:48:01 -0400

    Experimented more with the map view

--- GroupTableViewApp/GroupTableViewApp/MapViewController.swift
+++ GroupTableViewApp/GroupTableViewApp/MapViewController.swift
@@ -8,16 +8,70 @@
 
 import UIKit
 import MapKit
-class MapViewController: UIViewController {
+class MapViewController: UIViewController{
 
+   
     @IBOutlet var mapView: MKMapView!
+    
+    @IBOutlet var longPress: UILongPressGestureRecognizer!
+    var loc:String!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let locationManager = CLLocationManager()
+        let userLocation = locationManager.location?.coordinate
+        setMapFocus(location: userLocation, radiusInKm: 250)
+        
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
-
+    func setMapFocus(location: CLLocationCoordinate2D?, radiusInKm radius: CLLocationDistance) {
+        
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location! ,latitudinalMeters: radius,longitudinalMeters: radius)
+        self.mapView.setRegion(region, animated: false)
+    }
+    
+    @IBAction func longDetected(_ sender: UILongPressGestureRecognizer) {
+        
+        if longPress.state == .began {
+        
+        let position = mapView.convert(sender.location(in: nil),toCoordinateFrom: mapView)
+        let center = CLLocation(latitude: position.latitude, longitude: position.longitude)
+        let geocoder = CLGeocoder()
+        
+        // Look up the location and pass it to the completion handler
+        
+            
+                
+            // Look up the location and pass it to the completion handler
+            geocoder.reverseGeocodeLocation(center,
+                        completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    let artwork = Artwork(title: "\(firstLocation?.name ?? "Nil")",
+                      locationName: "\(firstLocation?.name ?? "Nil")",
+                      discipline: "Building",
+                      coordinate: CLLocationCoordinate2D(latitude: position.latitude, longitude: position.longitude))
+                    
+                    self.mapView.addAnnotation(artwork)
+                }
+                else {
+                 // An error occurred during geocoding.
+                    print("Error")
+                }
+            })
+        
+            
+         
+        }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
